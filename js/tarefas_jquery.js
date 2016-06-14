50e17c3d89862410ed9a67ad459503d15f791bfa
Felipe Leme <lemefe21@gmail.com>
2016-06-13 21:24:05 -0300

    Tratando duas funções no mesmo elemento e implementando a função de salvar a tarefa editada

--- js/tarefas_jquery.js
+++ js/tarefas_jquery.js
@@ -5,15 +5,30 @@ $(function(){
   function onTarefaDeleteClick() {
     //console.log($(this).parent(".tarefa-item").text().trim());
     //this - elemento .tarefa-delete
-    $(this).parent(".tarefa-item").hide("slow", function(){
+    $(this).parent(".tarefa-item").off("click").hide("slow", function(){
       //this - elemento .tarefa-lista
       $(this).remove();
     });
   }
 
-  function savePendingEdition(tarefa) {
-    console.log("Aqui vamos salvar nossa tarefa sendo editada!");
+  function savePendingEdition($tarefa) {
+
     //salva a tarefa
+    console.log("Aqui vamos salvar nossa tarefa sendo editada!");
+    //texto da tag criada ao clicar no elemento em onTarefaItemClick
+    var text = $tarefa.children(".tarefa-edit").val();
+    //remove o elemento
+    $tarefa.empty();
+
+    //monta um novo elemento tarefa
+    $tarefa.append("<div class='tarefa-texto'>" + text + "</div>")
+    .append("<div class='tarefa-delete'></div>")
+    .append("<div class='clear'></div>");
+
+    //declara os eventos para esse elemento criado
+    $(".tarefa-delete").click(onTarefaDeleteClick);
+    $tarefa.click(onTarefaItemClick);
+
   }
 
   function onTarefaEditKeyDown(event) {
@@ -36,6 +51,7 @@ $(function(){
 
       $lastClicked = $(this);
 
+      //pega o texto do elemento clicado
       var text = $lastClicked.children(".tarefa-texto").text();
       var html = "<input type='text' class='tarefa-edit' value='" + text + "'>";
 
